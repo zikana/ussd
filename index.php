e075f73043d52189f3dd6123a48e9c197fde4393
zikana <56475756+zikana@users.noreply.github.com>
2022-08-23 12:37:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,35 @@
 <?php
-$sessionID=$_POST['sessionid'];
-$severid =$_POST['serverid'];
- $phoneNumber=$_POST['phonenumebr'];
- $text=$_POST['text'];
+// Read the variables sent via POST from our API
+$sessionId   = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
+$phoneNumber = $_POST["phoneNumber"];
+$text        = $_POST["text"];
 
- if ($text ==""){
-    //  first request 
+if ($text == "") {
+    // This is the first request. Note how we start the response with CON
+    $response  = "CON What would you want to check \n";
+    $response .= "1. My Account \n";
+    $response .= "2. My phone number";
 
-    $response ="CON what would you like to check \n";
-     $response .= "1. Account number \n";
-     $response .="2. Phone number \n";
- }elseif($text =='1'){
-   $response ="CON choose account Information";
-   $response .= "1. Account number \n";
-   $response .="2. Account balance \n";
- }
- elseif($text =='2'){
-    $response ="End your phone number is ".$phoneNumber;
-  }
-  elseif($text =="1*1"){
-    $accountnumber= "ACC1001";
-    $response ="End your account number is ".$accountnumber;
-  }
-  elseif($text =='1*2'){
-    $balance= "K400";
-    $response ="End your Balance is ".$balance;
-  }
+} else if ($text == "1") {
+    // Business logic for first level response
+    $response = "CON Choose account information you want to view \n";
+    $response .= "1. Account number \n";
 
-  $header('content-type;text/plain');
-  echo $response;
-  ?>
+} else if ($text == "2") {
+    // Business logic for first level response
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your phone number is ".$phoneNumber;
+
+} else if($text == "1*1") { 
+    // This is a second level response where the user selected 1 in the first instance
+    $accountNumber  = "ACC1001";
+
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your account number is ".$accountNumber;
+
+}
+
+// Echo the response back to the API
+header('Content-type: text/plain');
+echo $response;
